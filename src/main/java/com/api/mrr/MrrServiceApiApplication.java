061fde1d405eacb5ea6289950e8e9ddd2ea8ca38
Bazukaaa <60100309+Bazukaaa@users.noreply.github.com>
2022-05-14 16:47:04 +0700

    first commit

---
+++ src/main/java/com/api/mrr/MrrServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.mrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrrServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrrServiceApiApplication.class, args);
+    }
+
+}
